ab8dd13163ad4992112aac39f70d11fd86679ff3
AlexC1991 <114543336+AlexC1991@users.noreply.github.com>
2022-09-28 11:58:36 +1000

    Week 2 Activities

---
+++ Assets/Scripts/WeekTwoActivites.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Experimental.AI;
+
+namespace AlexzanderCowell
+{
+    public class WeekTwoActivites : MonoBehaviour
+{   
+        [SerializeField] private string favouriteGame = "World of Tanks";
+        [SerializeField] private int hoursPlayed = 5000;
+        [SerializeField] private int gamePrice = 0; 
+        // Start 
+        void Start()
+        {
+
+            Debug.Log("My favourite game is " + favouriteGame);
+            Debug.Log("Hours i have Played is " + hoursPlayed);
+            Debug.Log("The Price of the Game is " + gamePrice);
+        }
+
+    }
+}
+
